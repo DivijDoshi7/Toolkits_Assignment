fd6faa3e46a7c96f4f2e7729ca032991e623cb46
Divij Doshi <divijdoshi@gmail.com>
2023-09-18 20:11:05 +0530

    Final Updates

--- test_code.py
+++ test_code.py
@@ -1,5 +1,23 @@
-file_path = "/Users/divijdoshi/Desktop/COMP219_Git_sample/sample_data1.txt"
+import csv
+import pandas as pd
 
-with open(file_path, 'r') as file:
-	for line in file:
-		print(line.strip())
+job_titles = ["Data Architect", "Senior Business Analyst", "Data Scientist", "Machine Learning Engineer"]
+csv_file_path = "/Users/divijdoshi/Desktop/COMP219_Git_sample/job_data.csv"
+
+try:
+    job_data = pd.read_csv(csv_file_path)
+    filtered_data = job_data[job_data['job_title'].isin(job_titles)]
+    grouped_data = filtered_data.groupby('job_title')['num_of_salaries']
+
+    for title in job_titles:
+        if title in grouped_data.groups:
+            average_salary = grouped_data.get_group(title).mean()
+            print(f"Average num_of_salaries for {title}: {average_salary:.2f}")
+        else:
+            print(f"No data found for {title}")
+
+except FileNotFoundError:
+    print(f"File '{csv_file_path}' not found.")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
